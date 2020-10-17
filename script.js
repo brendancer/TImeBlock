83752f0100a515778ec2e8ece3ff5536ddbb067f
Brenda Goodwin <brendanced@gmail.com>
2020-10-17 15:17:45 -0600

    basic construction and styleing of timeblock

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 // Insert Date
 var date = moment().format("LLLL");
-console.log(date);
+console.log(Date);
 document.getElementById("currentDay").textContent = date;
+
+function saveAppt() {
+  $("button").click(function () {
+    var appt = $.trim($("#appointment").val());
+    if (appt != "") {
+      localStorage.setItem("time", appt);
+      console.log(appt);
+    }
+  });
+
+  function restore() {
+    var oldappt = localStorage.getItem("appt");
+    if (appt != null)
+      document.getElementById("savedAppt").innerHTML = "Don't Forget" + oldappt;
+    console.log(oldappt);
+  }
+
+  //save info in local Storage
+  //on refresh of page , retrieve and print local storage
+}
