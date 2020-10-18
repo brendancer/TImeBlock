0cccdcef2fc6c9541a273f89fb6433d051202ce9
Brenda Goodwin <brendanced@gmail.com>
2020-10-17 18:59:59 -0600

    local storage working

--- script.js
+++ script.js
@@ -5,29 +5,24 @@ document.getElementById("currentDay").textContent = date;
 
 // get appointment text entered into textarea and connect it to time id
 
-var savebtn = document.getElementsByClassName("sbtn");
-savebtn.addEventListener("click", saveAppt);
-function saveAppt() {
-  var appt = this.appointment.value;
+$(".sbtn").on("click", function () {
+  var appt = $(this).siblings(".appointment").val();
   console.log(appt);
-  var id = document.getElementsByClassName(appointment).parentElement.id;
-  time = id;
+  var time = $(this).parent().attr("id");
+
   console.log(time);
   //add to local storage
   if (appt != "") {
     localStorage.setItem(time, appt);
     console.log(appt);
   }
-}
+});
 
-//load the saved data from local storage
+// //load the saved data from local storage
 
-function restore() {
-  var oldappt = localStorage.getItem("appt");
-  if (appt != null)
-    document.getElementById("savedAppt").innerHTML = "Don't Forget" + oldappt;
-  console.log(oldappt);
-}
-
-//save info in local Storage
-//on refresh of page , retrieve and print local storage
+// function restore() {
+//   var oldappt = localStorage.getItem("appt");
+//   if (appt != null)
+//     document.getElementById("savedAppt").innerHTML = "Don't Forget" + oldappt;
+//   console.log(oldappt);
+// }
