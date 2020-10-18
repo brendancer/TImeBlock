8e5a6d8df0ed0f92baa198a6b81bca2b79683f7a
Brenda Goodwin <brendanced@gmail.com>
2020-10-17 18:21:41 -0600

    trying to write to local storage

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-// Insert Date
+//Insert Date
 var date = moment().format("LLLL");
 console.log(Date);
 document.getElementById("currentDay").textContent = date;
 
+// get appointment text entered into textarea and connect it to time id
+
+var savebtn = document.getElementsByClassName("sbtn");
+savebtn.addEventListener("click", saveAppt);
 function saveAppt() {
-  $("button").click(function () {
-    var appt = $.trim($("#appointment").val());
-    if (appt != "") {
-      localStorage.setItem("time", appt);
-      console.log(appt);
-    }
-  });
-
-  function restore() {
-    var oldappt = localStorage.getItem("appt");
-    if (appt != null)
-      document.getElementById("savedAppt").innerHTML = "Don't Forget" + oldappt;
-    console.log(oldappt);
+  var appt = this.appointment.value;
+  console.log(appt);
+  var id = document.getElementsByClassName(appointment).parentElement.id;
+  time = id;
+  console.log(time);
+  //add to local storage
+  if (appt != "") {
+    localStorage.setItem(time, appt);
+    console.log(appt);
   }
-
-  //save info in local Storage
-  //on refresh of page , retrieve and print local storage
 }
+
+//load the saved data from local storage
+
+function restore() {
+  var oldappt = localStorage.getItem("appt");
+  if (appt != null)
+    document.getElementById("savedAppt").innerHTML = "Don't Forget" + oldappt;
+  console.log(oldappt);
+}
+
+//save info in local Storage
+//on refresh of page , retrieve and print local storage
